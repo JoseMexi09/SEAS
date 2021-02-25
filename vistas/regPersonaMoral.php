7366338dcbd9db4e224c0f790df335f34325673c
JoseMexi09 <78827406+JoseMexi09@users.noreply.github.com>
2021-02-25 13:12:33 -0600

    Add files via upload

---
+++ vistas/regPersonaMoral.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+if (isset($_SESSION['usuario'])){
+include "header_regPersonaMoral.php";
+
+ ?>
+<div class="container">
+	<div class="row">
+		<div class="col-small-12">
+			
+
+		</div>
+	</div>
+</div>
+
+
+
+
+<?php
+//include "footer.php";
+}else{
+	header("location:../index.php");
+}
+
+ ?>
